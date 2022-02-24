2c681132b92f470f66ff74c84ace0bffa76aabac
Jose M. Puebla <joey_manuel@outlook.com>
2022-02-23 21:30:29 -0500

    3. Third Commit
    - Set the original position of the player.

---
+++ My project/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    #region Variables
+
+    private Transform playerPosition;
+    #endregion Variables
+
+
+
+    #region Unity_Functions
+    private void Start()
+    {
+        // take the current position = new position (0,0,0)
+        transform.position = new Vector3(0, 0, 0);
+        
+    }
+
+
+    private void Update()
+    {
+        
+    }
+    #endregion Unity_Functions
+}
